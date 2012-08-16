d836f9912011ba55552fba27abbc027eefe5c1b8
Blueharford <blueharford@gmail.com>
2012-08-16 11:57:26 -0700

    Revert "Framework : Lockscreen - Customizable shortcuts"
    This reverts commit b80c13e2e01425b3e59acfa502a45d3fc779c9f3.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1910,12 +1910,6 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
-         * Stores values for custom lockscreen targets
-         * @hide
-         */
-        public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
-
-        /**
          * URI for the low battery sound file.
          * @hide
          */
